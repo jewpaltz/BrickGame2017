ed1e293609de0b94d514d6d1b712ac9d199ef859
Moshe Plotkin <mplotkin@jewpaltz.com>
2017-03-06 11:06:59 -0500

    Show Game Over and keep track of lives

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
-    public Vector2 startingVelocity = new Vector2(15, -20);
+    public  Vector2 startingVelocity = new Vector2(15, -20);
+    private Vector3 startingPosition;
+    public  GameObject gameOver;
+
+    int lives = 3;
 
     void Start () {
+        startingPosition = transform.position;
         GetComponent<Rigidbody2D>().velocity = startingVelocity;
 	}
 	
@@ -15,10 +20,27 @@ public class Ball : MonoBehaviour {
 		if(transform.position.y < -3.5f) {
             GetOut();
         }
-	}
+        if (Input.GetButtonDown("Jump")) {
+            GetComponent<Rigidbody2D>().velocity = startingVelocity;
+        }
+    }
 
     void GetOut()
     {
         Debug.Log("You are out");
+        lives = lives - 1;
+
+        transform.position = startingPosition;
+        GetComponent<Rigidbody2D>().velocity = new Vector2();
+
+
+        if (lives == 0) {
+            DoGameOver();
+        }
+    }
+
+    void DoGameOver()
+    {
+        gameOver.SetActive(true);
     }
 }
